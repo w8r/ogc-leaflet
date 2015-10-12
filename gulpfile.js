38fbe679adac134eff31d6af3996c34e6d5ed8ff
w8r <info@w8r.name>
2015-10-12 08:36:54 +0200

    gulp production task

--- gulpfile.js
+++ gulpfile.js
@@ -21,50 +21,6 @@ gulp.task('less', function () {
 var dependencies = ['leaflet'];
 var scriptsCount = 0;
 
-// Private Functions
-// ----------------------------------------------------------------------------
-function bundleApp(watch) {
-  scriptsCount++;
-  // Browserify will bundle all our js files together in to one and will let
-  // us use modules in the front end.
-  var appBundler = browserify({
-      entries: 'index.js',
-      'ignore-missing': true,
-      standalone: 'ogc',
-      debug: true
-  });
-
-  if (watch){
-    // make the dependencies external so they dont get bundled by the
-    // app bundler. Dependencies are already bundled in vendor.js for
-    // development environments.
-    dependencies.forEach(function(dep){
-      appBundler.external(dep);
-    });
-  }
-
-  appBundler
-      // transform ES6 and JSX to ES5 with babelify
-      .transform(babel.configure({
-        optional: ['runtime']
-       }))
-      .bundle()
-      .on('error', gutil.log)
-      .on('log', function(msg) {
-        gutil.log(
-          gutil.colors.green('build:'),
-          gutil.colors.white(msg)
-        );
-      })
-      .pipe(source('ogc-leaflet-src.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({
-        loadMaps: true
-      }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./dist'));
-}
-
 function compile(watch) {
   var browserified = browserify('./index.js', {
       require: dependencies,
